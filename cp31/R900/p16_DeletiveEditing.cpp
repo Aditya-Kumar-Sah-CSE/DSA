320b2b682e0f5a78981139c5d96f4a6005c99959
Aditya Kumar Sah <adityakumarsah8709@gmail.com>
2026-01-07 22:22:33 +0530

    done

---
+++ cp31/R900/p16_DeletiveEditing.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+int t;
+cin>>t;
+while(t--){
+string s ,t;
+cin>>s>>t;
+int m = t.size();
+int n = s.size();
+vector<int> freq_t(26,0);
+ for (int i = 0; i < m; i++)
+{
+     freq_t[t[i] - 'A']++;
+}
+for (int i = n - 1; i >=0; i--)
+{
+    if(freq_t[s[i] - 'A']>0){
+     freq_t[s[i] - 'A']--;
+    }
+    else{
+     s[i] = '.';
+    }
+   
+}
+    
+string final = "";
+for(int i = 0;i<n; i++){
+     if(s[i] != '.')
+     final+=s[i];
+}
+
+if(final == t)
+cout<<"YES"<<endl;
+else
+cout<<"NO"<<endl;
+
+
+}
+
+
+
+
+}
+
+
