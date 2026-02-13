ced7fad2c9951dba7fdd0f06d76ca39a1b6d58aa
Aditya Kumar Sah <adityakumarsah8709@gmail.com>
2026-02-13 18:56:40 +0530

    done

---
+++ cp31/R800/p32_prefix_max.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int t;
+cin>>t;
+while(t--){
+int n;
+cin>>n;
+vector<int> a(n),b(n);
+
+for (int i = 0; i < n; i++)
+{
+    cin>>a[i];
+}
+int maxi = a[0];
+
+for (int i = 0; i < n; i++)
+{
+    maxi = max(maxi,a[i]);
+    
+}
+
+cout<<maxi*n<<endl;
+
+}
+
+
+
+
+}
