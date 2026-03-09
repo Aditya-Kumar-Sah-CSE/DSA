29607d795c030c302ceb8a654bfb730917aba581
Aditya Kumar Sah <adityakumarsah8709@gmail.com>
2026-03-09 19:27:48 +0530

    day6

--- leetcode/easy - codeStoryWithMik/242.valid-anagram.cpp
+++ leetcode/easy - codeStoryWithMik/242.valid-anagram.cpp
@@ -12,6 +12,37 @@ public:
         // sort(s.begin(),s.end());
         // sort(t.begin(),t.end());
         // return s==t;
+
+// ! Aproach2 : 26 size ka vector le ke frequency sab ka nikal mege 
+//! unicode character hoge to vector 128 or map lena - char , int 
+vector<int> count(26,0);
+for(auto &ch : s ) count[ch - 'a']++;
+for(auto &ch : t) count[ch -'a']--;
+
+//! method 1
+
+// for (int i = 0; i < 26; i++)
+// {
+//     if (count[i])
+//     {
+//         return false;
+//     }
+    
+// }
+
+// return true;
+
+//! method 2 lemda 
+
+bool allZeros = all_of(begin(count),end(count),[](int element){
+    return element == 0;
+});
+
+
+return allZeros;
+
+
+
     }
 };
 // @lc code=end
--- leetcode/easy - codeStoryWithMik/958.check-completeness-of-a-binary-tree.cpp
+++ leetcode/easy - codeStoryWithMik/958.check-completeness-of-a-binary-tree.cpp
@@ -18,13 +18,68 @@
  */
 class Solution {
 public:
+int countNode(TreeNode* root){
+if (root == NULL)
+{
+    return 0;
+}
+return 1+ countNode(root->left) + countNode(root->right);
+
+}
+
+bool dfs(TreeNode* root , int i,int tNode){
+    if (root== NULL)
+    {
+        return true;
+    }
+
+    if (i>tNode)
+    {
+        return false;
+    }
+    
+    return dfs(root->left,2*i,tNode) && dfs(root->right,2*i +1,tNode);
+    
+}
+
     bool isCompleteTree(TreeNode* root) {
         // BFS-if left is NULL but right child is their !!
         // 1. queue use krna h 
+        // if past me null dhekha to tu kha se aa gya bsdi
+//! Approach 1 - BFS
+
+        // queue<TreeNode*>que;
+
+        // que.push(root);
+
+        // bool past = false;
+
+        // while (!que.empty())
+        // {
+        //     TreeNode* node = que.front();
+//             que.pop();
+
+//             if(node == NULL) past = true;
+//             else{
+//                 if(past == true)
+//                 return false;
+
+//                 que.push(node->left);
+//                 que.push(node->right);
+//             }
+//         }
         
 
 
+// return true;
 
+//! Approach 2 - DFS -binary tree ko array me fill kr sakte h - left ka index = 2*i and right child me 2*i + 1
+
+// if max-index > number of node = [7,1,3,8,9,NULL,7]
+
+int tNode= countNode(root);
+int i=1;
+return dfs(root,i,tNode);
     }
 };
 // @lc code=end
