6aea54ebf32514aac93e090d855c764d326ca3e2
Aditya Kumar Sah <adityakumarsah8709@gmail.com>
2026-03-09 18:40:01 +0530

    day6

---
+++ leetcode/easy - codeStoryWithMik/236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=236 lang=cpp
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root==NULL) return NULL;
+        if (root == p || root == q)
+        {
+            return root;
+        }
+
+        
+     TreeNode* leftN = lowestCommonAncestor(root->left,p,q);
+     TreeNode* rightN = lowestCommonAncestor(root->right,p,q);
+if (leftN != NULL && rightN != NULL)
+{
+    return root;
+}
+if (leftN!=NULL)
+{
+    return leftN;
+}
+
+
+return rightN;
+    }
+};
+// @lc code=end
+
---
+++ leetcode/easy - codeStoryWithMik/958.check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=958 lang=cpp
+ *
+ * [958] Check Completeness of a Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        // BFS-if left is NULL but right child is their !!
+        // 1. queue use krna h 
+        
+
+
+
+    }
+};
+// @lc code=end
+
